2d294cd1b60dc41867f129246d6e694d45a8f6b3
niza3001 <n.zarei.3001@gmail.com>
2016-05-03 11:09:29 -0500

    new commit

---
+++ js/SearchResults.js
@@ -0,0 +1,55 @@
+//create filter Area
+var filterOptionArea = document.createElement("div")
+filterOptionArea.id = "filterOptionArea";
+document.body.appendChild(filterOptionArea);
+
+//create header Area
+var header = document.createElement("div");
+header.id = "header";
+document.body.appendChild(header);
+
+//create search result Area
+var resultArea = document.createElement("div");
+resultArea.id = "resultArea";
+document.body.appendChild(resultArea);
+
+var urls = [];
+urls[0]=
+urls[1]=
+urls[2]=
+urls[3]=
+urls[4]=
+urls[5]=
+
+var newElement = document.createElement('div');
+    newElement.id = cars[c]; newElement.className = "car";
+    newElement.innerHTML = cars[c];
+    document.body.appendChild(newElement);
+
+for (var i in urls){
+
+
+}
+
+
+
+//create search result containers
+var resultBox1 = document.createElement("div");
+resultBox1.id = "resultBox1";
+resultBox1.class = "resultContainer"
+resultArea.appendChild(resultBox1);
+
+var resultBox2 = document.createElement("div");
+resultBox2.id = "resultBox2";
+resultBox2.class = "resultContainer"
+resultArea.appendChild(resultBox2);
+
+var resultBox3 = document.createElement("div");
+resultBox3.id = "resultBox3";
+resultBox3.class = "resultContainer"
+resultArea.appendChild(resultBox3);
+
+var resultBox4 = document.createElement("div");
+resultBox4.id = "resultBox4";
+resultBox4.class = "resultContainer"
+resultArea.appendChild(resultBox4);
