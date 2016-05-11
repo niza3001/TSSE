d311baee9b39fc93c918fe74faa12caf6c7ab059
niza3001 <n.zarei.3001@gmail.com>
2016-05-11 15:15:56 -0700

    data

---
+++ js/Data.js
@@ -0,0 +1,78 @@
+//name, model, Processor, RAM, HDD,screen size, weight, AmznRating, AmznURL,YoutubeURL
+var prdct1 {
+  "name" : "2016 HP Pavillion 15'"
+  "processor" : "core i5 2.2 GHz"
+  "Memory" : "4 GB DDR3 SDRAM"
+  "storage" : "1 TB 5400 RPM"
+  "weight" : "4.73 pounds"
+  "price" : "389"
+  "screen_size" : "15'"
+  "AmznRating" : "3.6"
+  "AmznURL" : "http://www.amazon.com/HP-Pavilion-15-6-inch-Processor-Webcam-Windows/dp/B01BLRZCRG/ref=sr_1_25?s=pc&ie=UTF8&qid=1463001194&sr=1-25&keywords=laptops"
+  "YoutubeURL" : ""
+}
+
+var prdct2 {
+  "name" : "2016 HP Probook"
+  "processor" : "1.8 GHz AMD A Series"
+  "Memory" : "16 GB SDRAM DDR3"
+  "storage" : "1 TB HDD 5400 rpm"
+  "weight" : "4.7 pounds"
+  "price" : "449"
+  "screen_size" : "15.6"
+  "AmznRating" : "5"
+  "AmznURL" : "http://www.amazon.com/HP-15-6-inch-Performance-Quad-Core-A10-8700P/dp/B01EY095EI/ref=sr_1_27?s=pc&ie=UTF8&qid=1463001194&sr=1-27&keywords=laptops"
+  "YoutubeURL" : ""
+}
+
+var prdct3 {
+  "name" : "HP Envy 15t"
+  "processor" : "core i7 2.6 GHz"
+  "Memory" : "16 GB DDR3 1600"
+  "storage" : "2 TB"
+  "weight" : "5.23 pounds"
+  "price" : "869.99"
+  "screen_size" : "15.6"
+  "AmznRating" : "3.3"
+  "AmznURL" : "http://www.amazon.com/dp/B016QS29T4/ref=psdc_565108_t1_B016QOQ4M6"
+  "YoutubeURL" : ""
+}
+
+var prdct4 {
+  "name" : "HP Business 15t"
+  "processor" : "amd a series 2.2 GHz"
+  "Memory" : "4 GB DDR3 SDRAM"
+  "storage" : "1 TB 5400 RPM"
+  "weight" : "4.73 pounds"
+  "price" : "362.95"
+  "screen_size" : ""
+  "AmznRating" : "15.6"
+  "AmznURL" : "http://www.amazon.com/HP-Touchscreen-TouchScreen-SuperMulti-3-0-Win10/dp/B01B16TVH4/ref=sr_1_47?s=pc&ie=UTF8&qid=1463001194&sr=1-47&keywords=laptops"
+  "YoutubeURL" : ""
+}
+
+var prdct5 {
+  "name" : "HP Elitebook"
+  "processor" : "2.4 GHz Intel Core 2 Duo"
+  "Memory" : "2 GB SDRAM DDR2"
+  "storage" : "160 GB"
+  "weight" : "5 pounds"
+  "price" : "149.98"
+  "screen_size" : "14"
+  "AmznRating" : "3.5"
+  "AmznURL" : "http://www.amazon.com/HP-Elitebook-6930p-LAPTOP-WINDOWS/dp/B00SI03GS2/ref=sr_1_15?s=pc&ie=UTF8&qid=1463003764&sr=1-15&keywords=laptops"
+  "YoutubeURL" : ""
+}
+
+var prdct6 {
+  "name" : "HP Stream"
+  "processor" : "celeron n3050 1.6 GHz	"
+  "Memory" : "2 GB SDRAM"
+  "storage" : "0 GB	"
+  "weight" : "2.6 pounds"
+  "price" : "199.99"
+  "screen_size" : "11.6-Inch"
+  "AmznRating" : "3.9"
+  "AmznURL" : "http://www.amazon.com/HP-11-r010nr-11-6-Inch-Notebook-Processor/dp/B015CQ8SGE/ref=sr_1_21?s=pc&ie=UTF8&qid=1463003764&sr=1-21&keywords=laptops"
+  "YoutubeURL" : ""
+}
